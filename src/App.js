67037743332ec0edf70eb3dfc3246541ff47ff54
Forgotten You <maximleluk@gmail.com>
2021-07-04 13:16:03 +0300

    App barebones ready to engage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
 import Header from "./modules/Header";
 import Home from "./modules/Home";
 import Library from "./modules/Library";
